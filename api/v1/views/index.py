4b9df896649606bd4f4aacace14c86a204e64523
MissYetunde <surajudeenyetunde@gmail.com>
2024-02-25 07:17:35 -0800

    created endpints to retrive objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,27 @@
 
 from api.v1.views import app_views
 from flask import jsonify
-
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 @app_views.route('/status', methods=["GET"], strict_slashes=False)
 def status():
     """show the status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/status', methods=["GET"], strict_slashes=False)
+""" Endpoint that retrieves the number of each objects by type """
+def stats():
+    object_counts = {}
+    object_counts["amenities"] = storage.count(Amenity)
+    object_counts["cities"] = storage.count(City)
+    object_counts["places"] = storage.count(Place)
+    object_counts["reviews"] = storage.count(Review)
+    object_counts["states"] = storage.count(State)
+    object_counts["users"] = storage.count(User)
+    return jsonify(object_counts)
